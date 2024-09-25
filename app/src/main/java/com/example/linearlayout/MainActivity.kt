222c3476e3fac4444d6b4f9e854b86249f9df10a
circumstancesl <145998442+circumstancesl@users.noreply.github.com>
2024-09-25 15:29:03 +0300

    added MainActivity.kt

--- app/src/main/java/com/example/linearlayout/MainActivity.kt
+++ app/src/main/java/com/example/linearlayout/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.linearlayout
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button = findViewById<Button>(R.id.button_ok)
+        button.setOnClickListener {
+            Toast.makeText(this, "Кнопка ОК", Toast.LENGTH_SHORT).show()
         }
     }
 }
