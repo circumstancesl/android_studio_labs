55f6f037d9ae17f89c7d15bce0fdd7043bb66230
circumstancesl <145998442+circumstancesl@users.noreply.github.com>
2024-09-25 17:00:32 +0300

    added MainActivity,kt

--- app/src/main/java/com/example/linearlayout/MainActivity.kt
+++ app/src/main/java/com/example/linearlayout/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.linearlayout
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val editText: EditText = findViewById(R.id.editText)
+        editText.setText("")
+        val buttonLog: Button = findViewById(R.id.button_log)
+
+        buttonLog.setOnClickListener {
+            val text = editText.text.toString()
+            Log.v("From EditText: ", text)
+        }
+
+        Timber.plant(Timber.DebugTree())
+        val buttonTimber: Button = findViewById(R.id.button_timber)
+
+        buttonTimber.setOnClickListener {
+            val text = editText.text.toString()
+            Timber.v(text)
         }
     }
 }
