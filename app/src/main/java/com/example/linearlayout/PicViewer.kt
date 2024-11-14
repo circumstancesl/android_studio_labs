3702f25ccdd62667a6356511f8163defc2f9fb94
circumstancesl <145998442+circumstancesl@users.noreply.github.com>
2024-11-14 17:30:27 +0300

    activiry

---
+++ app/src/main/java/com/example/linearlayout/PicViewer.kt
@@ -0,0 +1,56 @@
+package com.example.linearlayout
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+
+class PicViewer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.pic_viewer)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val picLink = intent.getStringExtra("picLink")
+        val picView: ImageView = findViewById(R.id.picView)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+        Glide.with(this).load(picLink).into(picView)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.favorite) {
+            Toast.makeText(this, "Добавлено в Избранное", Toast.LENGTH_SHORT).show()
+
+            val picLink = intent.getStringExtra("picLink")
+            val resultIntent = Intent().putExtra("picLink", picLink)
+
+            setResult(RESULT_OK, resultIntent)
+            finish()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
