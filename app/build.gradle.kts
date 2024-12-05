8d8738de2aa1bb9ff54e7665f03cc2024908849a
circumstancesl <145998442+circumstancesl@users.noreply.github.com>
2024-12-05 16:11:21 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,10 @@ android {
 
 dependencies {
 
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
