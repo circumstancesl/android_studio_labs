8098d3bed5b10373c906af2366703c78dc91c2b4
circumstancesl <145998442+circumstancesl@users.noreply.github.com>
2024-12-05 16:23:56 +0300

    new code

--- app/src/main/java/com/example/linearlayout/MainActivity.kt
+++ app/src/main/java/com/example/linearlayout/MainActivity.kt
@@ -3,18 +3,142 @@ package com.example.linearlayout
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.r_view)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val recyclerView: RecyclerView = findViewById(R.id.r_view)
+        var adapter: ForecastAdapter = ForecastAdapter(ForecastDiffCallback())
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(OpenWeatherMapService::class.java)
+
+        val apiKey = resources.getString(R.string.key)
+        val city = "Шклов"
+        val units = "metric"
+
+        val call = service.getForecast(city, units, apiKey)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        call.enqueue(object : Callback<Forecast> {
+            override fun onResponse(call: Call<Forecast>, response: Response<Forecast>) {
+                if (response.isSuccessful) {
+                    val forecast = response.body()
+                    adapter.submitList(forecast!!.list)
+                }
+                else
+                {
+
+                }
+            }
+
+            override fun onFailure(call: Call<Forecast>, t: Throwable) {
+            }
+        })
+    }
+}
+
+interface OpenWeatherMapService {
+    @GET("forecast")
+    fun getForecast(
+        @Query("q") city: String,
+        @Query("units") units: String,
+        @Query("appid") apiKey: String
+    ): Call<Forecast>
+}
+
+data class Main(
+    val temp: Double,
+    val feels_like: Double,
+    val temp_min: Double,
+    val temp_max: Double,
+    val pressure: Int,
+    val humidity: Int
+)
+
+data class Weather(
+    val id: Int,
+    val main: String,
+    val description: String,
+    val icon: String
+)
+
+data class Clouds(
+    val all: Int
+)
+
+data class Wind(
+    val speed: Double,
+    val deg: Int,
+    val gust: Double
+)
+
+data class City(
+    val id: Int,
+    val name: String,
+    val coord: Coord,
+    val country: String,
+    val population: Int
+)
+
+data class Coord(
+    val lat: Double,
+    val lon: Double
+)
+
+data class Forecast(
+    val cod: String,
+    val message: Double,
+    val cnt: Int,
+    val list: List<ForecastItem>,
+    val city: City
+)
+
+data class ForecastItem(
+    val dt: Int,
+    val main: Main,
+    val weather: List<Weather>,
+    val clouds: Clouds,
+    val wind: Wind,
+    val dt_txt: String
+)
+
+class ForecastDiffCallback : DiffUtil.ItemCallback<ForecastItem>() {
+    override fun areItemsTheSame(oldItem: ForecastItem, newItem: ForecastItem): Boolean {
+        return oldItem.dt_txt == newItem.dt_txt
+    }
+
+    override fun areContentsTheSame(oldItem: ForecastItem, newItem: ForecastItem): Boolean {
+        return oldItem == newItem
     }
 }
